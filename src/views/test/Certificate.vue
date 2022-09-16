08f9e127ee0ae12b905ee65c840a53d10ad67d9d
jeffkim <jeff.kim@medibloc.org>
2022-09-16 16:25:16 +0900

    제증명서 페이지 > 발급대기 테이블 생성완료

--- src/views/test/Certificate.vue
+++ src/views/test/Certificate.vue
@@ -1,112 +1,130 @@
 <template>
   <div class="card">
-    <DataTable
-      :value="products"
-      v-model:expandedRows="expandedRows"
-      dataKey="id"
-      responsiveLayout="scroll"
-      @row-click="openUserData($event.data.id)"
-    >
-      <Column header="번호" bodyStyle="text-align: center">
-        <template #body="slotProps">
+    <TabView ref="tabview1">
+      <TabPanel header="발급대기">
+        <DataTable
+          :value="products"
+          v-model:expandedRows="expandedRows"
+          dataKey="id"
+          responsiveLayout="scroll"
+          @row-click="openUserData($event.data.id)"
+        >
+          <Column header="번호" field="id" bodyStyle="text-align: center">
+            <!-- <template #body="slotProps">
           {{ slotProps.index }}
-        </template>
-      </Column>
-      <Column
-        field="name"
-        header="환자명(휴대폰)"
-        bodyStyle="text-align: center"
-      ></Column>
-      <Column header="완료/총신청수" bodyStyle="text-align: center">
-        <template #body="slotProps">
-          <img
-            src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
-            :alt="slotProps.data.image"
-            class="product-image"
-          />
-        </template>
-      </Column>
-      <Column field="price" header="진료과" bodyStyle="text-align: center">
-        <template #body="slotProps">
-          {{ formatCurrency(slotProps.data.price) }}
-        </template>
-      </Column>
-      <Column
-        field="category"
-        header="의료진"
-        bodyStyle="text-align: center"
-      ></Column>
-      <Column field="rating" header="진료일" bodyStyle="text-align: center">
-        <template #body="slotProps">
-          <Rating
-            :modelValue="slotProps.data.rating"
-            :readonly="true"
-            :cancel="false"
-          />
-        </template>
-      </Column>
-      <Column
-        field="inventoryStatus"
-        header="용도"
-        bodyStyle="text-align: center"
-      >
-        <template #body="slotProps">
-          <span
-            :class="
-              'product-badge status-' +
-              slotProps.data.inventoryStatus.toLowerCase()
-            "
-            >{{ slotProps.data.inventoryStatus }}</span
+        </template> -->
+          </Column>
+          <Column
+            field="userNm"
+            header="환자명(휴대폰)"
+            bodyStyle="text-align: center"
+          ></Column>
+          <Column header="완료/총신청수" bodyStyle="text-align: center">
+            <template #body="slotProps">
+              {{ checkStatus(slotProps.data.applicationCert) }} /
+              {{ slotProps.data.applicationCert.length }}
+            </template>
+          </Column>
+          <Column
+            field="department"
+            header="진료과"
+            bodyStyle="text-align: center"
+          ></Column>
+          <Column
+            field="medicalstaff"
+            header="의료진"
+            bodyStyle="text-align: center"
+          ></Column>
+          <Column
+            field="diagnosisData"
+            header="진료일"
+            bodyStyle="text-align: center"
           >
-        </template>
-      </Column>
-      <Column header="신청일" bodyStyle="text-align: center">
-        <template #body="slotProps">
-          {{
-            slotProps.data.orders.length !== 0
-              ? slotProps.data.orders[0].date
-              : "2022-09-02"
-          }}
-        </template>
-      </Column>
-      <!--expansion datas-->
-      <template #expansion="slotProps">
-        <div class="orders-subtable">
-          <div class="card">
-            <Button label="닫기" @click="collapseAll"></Button>
-            <div>생년월일 : 1990.03.23 (여)</div>
-            <div>{{ slotProps.data.name }}</div>
-            <div>{{ slotProps.data.description }}</div>
-            <div>{{ slotProps.data.category }}</div>
-          </div>
-        </div>
-      </template>
-      <!--expansion datas-->
-    </DataTable>
+          </Column>
+          <Column field="purpose" header="용도" bodyStyle="text-align: center">
+          </Column>
+          <Column
+            field="reqDate"
+            header="신청일"
+            bodyStyle="text-align: center"
+          >
+          </Column>
+          <!--expansion datas-->
+          <template #expansion="slotProps">
+            <div class="orders-subtable">
+              <div class="card">
+                <div class="grid">
+                  <!--1-->
+                  <div class="col-5 font-bold text-xl">
+                    <div>
+                      생년월일: {{ slotProps.data.userBirth }} ({{
+                        slotProps.data.userGender === "F" ? "여" : "나"
+                      }})
+                    </div>
+                    <div>신청일: {{ slotProps.data.reqDate }}</div>
+                    <div>진료일: {{ slotProps.data.diagnosisData }}</div>
+                  </div>
+                  <!--1-->
+                  <!--2-->
+                  <div class="col-6 font-bold text-xl">
+                    <div>휴대폰: {{ slotProps.data.userPhone }}</div>
+                    <div>
+                      발급일/내역없음처리일:
+                      {{
+                        slotProps.data.resData === ""
+                          ? "-"
+                          : slotProps.data.resData
+                      }}
+                    </div>
+                    <div>용도: {{ slotProps.data.purpose }}</div>
+                  </div>
+                  <!--2-->
+                  <!--3-->
+                  <div class="col-1">
+                    <div class="flex align-items-end justify-content-end">
+                      <Button
+                        label="닫기"
+                        @click="closeUserData(slotProps.data.id)"
+                      ></Button>
+                    </div>
+                  </div>
+                  <!--3-->
+                </div>
+              </div>
+            </div>
+          </template>
+          <!--expansion datas-->
+        </DataTable>
+      </TabPanel>
+      <TabPanel header="발급완료">
+        <p>생성전</p>
+      </TabPanel>
+      <TabPanel header="내역없음">
+        <p>생성전</p>
+      </TabPanel>
+    </TabView>
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
-import ProductService from "@/service/ProductService";
-import exp from "constants";
+// import ProductService from "@/service/ProductService";
 
+import certificateData from "@/tmp/certificate/waitingIssu.json";
 export default {
   setup() {
     onMounted(() => {
-      productService.value
-        .getProductsWithOrdersSmall()
-        .then((data) => (products.value = data));
+      products.value = certificateData.resultData;
     });
 
     const products = ref([]);
-    const productService = ref(new ProductService());
     const expandedRows = ref([]);
 
-    const collapseAll = () => {
-      expandedRows.value = [];
+    const closeUserData = (id) => {
+      console.log("close", id);
+      expandedRows.value = expandedRows.value.filter((p) => p.id !== id);
     };
-    
+
     const formatCurrency = (value) => {
       return value.toLocaleString("en-US", {
         style: "currency",
@@ -114,25 +132,31 @@ export default {
       });
     };
 
-    const openUserData = (id) => {
-      let nextData = products.value.filter((p) => p.id === id);
+    const openUserData = (idx) => {
+      let nextData = products.value.filter((p) => p.id === idx);
       let firstData = expandedRows.value[0];
       if (firstData) {
-        expandedRows.value = [
-          ...expandedRows.value,
-          nextData[0]
-        ]
+        expandedRows.value = [...expandedRows.value, nextData[0]];
       } else {
         expandedRows.value = nextData;
       }
     };
 
+    const checkStatus = (certStatus) => {
+      let cnt = 0;
+      for (let i = 0; i < certStatus.length; i++) {
+        if (certStatus[i].status === "Y") cnt++;
+      }
+      return cnt;
+    };
+
     return {
       products,
       expandedRows,
-      collapseAll,
+      closeUserData,
       formatCurrency,
       openUserData,
+      checkStatus,
     };
   },
 };
